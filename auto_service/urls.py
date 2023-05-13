57770f77a6ae607b68ab0390a800717b6d8da90b
multibala <qmultibala@gmail.com>
2023-05-13 14:06:27 +0600

    first apis

--- auto_service/urls.py
+++ auto_service/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/',include('auth.urls')),
+    path('check/',include('check.urls')),
+    path('dealer/',include('dealer.urls')),
+    path('lookup/',include('lookup.urls')),
+    path('reserve/',include('reserve.urls')),
+    path('user/',include('user.urls')),
+    
 ]
---
+++ check/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+urlpatterns =[
+    path('checkEmail',views.check_email),
+    path('checkUsername',views.checkUsername),
+    path('email/:token',views.email_token),
+    path('login',views.login),
+    path('refreshToken',views.refreshToken),
+    path('register',views.register),
+]
